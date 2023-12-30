bda4ad5d4dcea7536e4fabbec4c8ffb302012894
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-12-30 17:52:57 -0500

    Remove top lists

--- college-baseball-app/src/App.jsx
+++ college-baseball-app/src/App.jsx
@@ -1,28 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { Grid, CssBaseline, Box, Container } from '@mui/material'
+import { CssBaseline, Box } from '@mui/material'
 import { collection, query, where, getDocs } from "firebase/firestore"; 
 import { firestore } from '../firebase'; // Ensure this path is correct
 import Header from "./Header.jsx"
 import Player from "./Player.jsx"
-import TopList from "./TopList.jsx"
 import Search from "./Search.jsx"
 
 export default function App() {
   const [playerData, setPlayerData] = useState(null)
   const [multipleSearchResults, setMultipleSearchResults] = useState([]);
-  const [topListData, setTopListData] = useState({
-    hr: [],
-    ba: [],
-    ops: [],
-    bb: [],
-    era: [],
-    k: [],
-    kp: [],
-    baa: []
-  });
-
 
   const theme = createTheme({
     palette: {
@@ -103,42 +91,6 @@ export default function App() {
     }
   }
   
-  
-
-  function handleTopLists() {
-    const routes = [
-      "top-hr",
-      "top-ba",
-      "top-ops",
-      "top-bb",
-      "top-era",
-      "top-k",
-      "top-kp",
-      "top-baa"
-    ];
-  
-    const fetchPromises = routes.map(route => fetch(`http://localhost:3000/${route}`).then(res => res.json()));
-  
-    Promise.all(fetchPromises)
-      .then(dataArray => {
-        setTopListData({
-          hr: dataArray[0],
-          ba: dataArray[1],
-          ops: dataArray[2],
-          bb: dataArray[3],
-          era: dataArray[4],
-          k: dataArray[5],
-          kp: dataArray[6],
-          baa: dataArray[7]
-        });
-      })
-      .catch(error => console.error(error));
-  }
-
-  useEffect(() => {
-    handleTopLists();
-  }, []);
-  
   return (
     <ThemeProvider theme={theme}>
       <Router>
@@ -150,38 +102,6 @@ export default function App() {
           <Routes>
             <Route path="/player/:stats_player_seq" element={<Player data={playerData} />} />
             <Route path="/search" element={<Search players={multipleSearchResults} />} />
-            <Route path="/" element={
-              <Container>
-                <Grid container justifyContent="space-between" mt={2}>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.hr} title="Top HR" statKey="HR"/>
-                  </Grid>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.ba} title="Top BA" statKey="batting_average"/>
-                  </Grid>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.ops} title="Top OPS" statKey="OPS"/>
-                  </Grid>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.bb} title="Top BB" statKey="walks"/>
-                  </Grid>  
-                </Grid>
-                <Grid container justifyContent="space-between" mt={2}>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.era} title="Top ERA" statKey="ERA"/>
-                  </Grid>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.k} title="Top K" statKey="strikeouts"/>
-                  </Grid>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.kp} title="Top K%" statKey="strikeout_percentage"/>
-                  </Grid>
-                  <Grid item xs={2.5}>
-                    <TopList data={topListData.baa} title="Top BAA" statKey="batting_average_against"/>
-                  </Grid>  
-                </Grid>
-              </Container>
-            } />
           </Routes>
         </Box>
       </Router>
--- college-baseball-app/src/Header.jsx
+++ college-baseball-app/src/Header.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 import { useRef } from 'react' 
 
 export default function Header({ onSearch }) {
-  // Ref to hold the input element for player name search
+ 
   const inputRef = useRef()
 
   const navigate = useNavigate();
-  // Handler for search button click, triggers the onSearch callback with the input value
+
   function handleSearch() {
     const name = inputRef.current.value;
-    onSearch(name, navigate);  // Pass navigate to the onSearch callback
+    onSearch(name, navigate); 
   }
   return (
     <Grid 
